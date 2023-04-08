3d19214d0d7d75a662692f2200314be3e15f0090
Erick-WG <erix.academix@gmail.com>
2023-04-08 14:32:47 +0300

    addon

--- SRC/Green_Voyage/login/views.py
+++ SRC/Green_Voyage/login/views.py
@@ -18,7 +18,7 @@ def signup(request):
             return redirect(reverse('user_view'))
     else:
         form = UserCreationForm()
-    return render(request, 'signup.html', {'form': form})
+    return render(request, 'login/signup.html', {'form': form})
 
 def login_view(request):
     if request.method == 'POST':
@@ -34,20 +34,20 @@ def login_view(request):
                 form.add_error(None, 'Invalid username or password')
     else:
         form = AuthenticationForm()
-    return render(request, 'login.html', {'form': form})
+    return render(request, 'login/login.html', {'form': form})
 
 @login_required
 def user_view(request):
-    return render (request, 'userac.html')
+    return render (request, 'login/userac.html')
 
 @login_required
 def home(request):
-    return render(request, 'index.html')
+    return render(request, 'login/index.html')
 
 @login_required
 def signout(request):
     logout(request)
-    return render(request, 'signout.html')
+    return render(request, 'login/signout.html')
 
 
 
