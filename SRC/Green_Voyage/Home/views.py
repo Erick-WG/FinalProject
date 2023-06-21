b0d7e57628b1029a9fb1bffb78956a4f84dd1909
Erick-WG <erix.academix@gmail.com>
2023-06-21 13:38:12 +0300

    auth configs (encountered an error)

--- SRC/Green_Voyage/Home/views.py
+++ SRC/Green_Voyage/Home/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render, redirect
 
 # importing generic views.
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-#importing our custom form.
-# from .forms import PostForm, EditForm
+
+# importing our custom form.
+# from .forms import DestinationCollectionForm, ActivityCreationForm
 # importing out models
 from .models import Destination, Activity
 #importing our lazy folder for redirecting successful links.
@@ -14,18 +15,33 @@ from django.urls import reverse_lazy
 # Home view class.
 class HomeView(ListView):
     model = Destination
-    template_name = 'home.html'
-    context_object_name = 'destinations'
+    template_name = 'index.html'
+    # context_object_name = 'destinations'
 
 # Destinations view class.
-class DestinationsView(DetailView):
+class DestinationsView(ListView):
     model = Destination
     template_name = 'destinations.html'
-    context_object_name = 'destination'
+    # success_url = reverse_lazy('destinations')
+    # context_object_name = 'destination'
+
+# businesses can create destinations the provide touring to.
+class CreateDestination(CreateView):
+    model = Destination
+    template_name = "create_destination.html"
+    # form_class = DestinationCollectionForm
 
 
+class CreateActivity(CreateView):
+    model = Activity
+    template_name = 'activity.html'
+    # form_class = ActivityCreationForm
 
 
+class DestinationsDeleteView(DeleteView):
+    model = Destination
+    template_name = 'delete_destination.html'
+    success_url = reverse_lazy('home')
 
 
 
