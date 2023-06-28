fae3ceaed0ba2dc86aefc3e5345878b5ab8e61ba
Erick-WG <erix.academix@gmail.com>
2023-06-28 16:41:42 +0300

    registered the user models to admin dash

--- SRC/Green_Voyage/Members/admin.py
+++ SRC/Green_Voyage/Members/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import UserManager, BaseUserManager, AbstractBaseUser
 
-
+admin.site.register('UserManager')
+admin.site.register('BaseUserManager')
+admin.site.register('AbstractBaseUser')
