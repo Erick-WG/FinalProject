2804669eb0e89dc399148fe29fbab73915c60d81
Erick-WG <erix.academix@gmail.com>
2023-07-17 15:53:32 +0300

    Delete files

--- SRC/Green_Voyage/Home/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from . import models
-# Register your models here.
-admin.site.register(models.HeroImage)
-admin.site.register(models.Destination)
-admin.site.register(models.Activity)
-admin.site.register(models.Testimonial)
-
--- SRC/Green_Voyage/Members/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from . import models # UserManager, BaseUserManager #, AbstractBaseUser
-
-# admin.site.register(models.UserManager)
-# admin.site.register(models.BaseUserManager)
-# admin.site.register(AbstractBaseUser)
