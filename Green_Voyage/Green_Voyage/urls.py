454ee0d92fe72176d3e3f4a1833b60a4ed14edb0
Erick-WG <erix.academix@gmail.com>
2023-07-20 00:12:36 +0300

    added the dependancies needed to run the project (pillow & Django)

--- Green_Voyage/Green_Voyage/urls.py
+++ Green_Voyage/Green_Voyage/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+import Members
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # have a home path
+    # path('', include('Members.urls')),
 ]
